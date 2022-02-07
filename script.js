128afc9451f4df9d50eddc1a4c3fc1ce73e596c6
Gpsnm <adam-willetts@hotmail.co.uk>
2022-02-07 18:19:42 +0000

    ui added

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
-// VAR FOR SCORING
-let playerWin = ("Player Wins This Round!");
-let computerWin = ("Computer Wins This Round!")
-let draw = ("Its A Draw!")
-let playerScore = [1];
-let computerScore = [1];
-let compScore = computerScore.push();
-let playScore = playerScore.push();
+ const computerChoiceDisplay = document.getElementById('computer-choice');
+ const userChoiceDisplay = document.getElementById('user-choice');
+ const result = document.getElementById('result');
+ const possibleChoices = document.querySelectorAll('button');
+ let userChoice;
+ let computerChoice
+ let results
+let playerScore = 1;
+let computerScore = 1;
 
 
-// FUNCTION TO CHOOSE RANDOM ARRAY ITEM 
-function computerPlay(){
-    const randomGame = ["rock","paper","scissors"];
-    let compRandom = randomGame[Math.floor(Math.random() * randomGame.length )];
-    return compRandom;
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+userChoice = e.target.id
+userChoiceDisplay.innerHTML = userChoice;
+generateComputerChoice()
+getResult();
+}))
+
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length + 1)
+    if (randomNumber === 1)
+    computerChoice = 'rock'
+    if (randomNumber === 2)
+    computerChoice = 'paper'
+    if (randomNumber === 3)
+    computerChoice = 'scissors'
+    computerChoiceDisplay.innerHTML = computerChoice;
 }
 
-// FUNCTION TO DISPLAY WINNER 
-function oneRound (playerSelection, computerSelection){
-    computerSelection = computerPlay();
-    playerSelection = prompt("Rock, Paper or Scissors").toLowerCase();
-    if (playerSelection == "paper" && computerSelection == "rock")
-        return (playerWin + " " + playScore++);
-        if (playerSelection == "rock" && computerSelection == "scissors")
-        return (playerWin + " " + playScore++);
-    if (playerSelection == "scissors" && computerSelection == "rock")
-        return (playerWin + " " + playScore++);
-    if ( computerSelection == playerSelection)
-    return (draw);
-    if(compScore === 5)
-    return ("the winner is the computer with " + " " + compScore + " points ")
-    if (playScore === 5)
-    return("the winner is the player with " + " " + playScore + " points ");
-    else 
-        return (computerWin + " " + compScore++);
-
-}
-
-// function to loop multiple rounds
-function Game(){
-    for (i = 0; i < 20; i++)
-return(oneRound());
-
+function getResult(){
+    if (computerChoice === userChoice)
+    results = 'its a draw!'
+    if (computerChoice === 'rock' && userChoice === 'paper')
+    results = 'You win!' + " " + playerScore++ + " point to the player"
+    if (computerChoice === 'paper' && userChoice == 'scissors')
+    results = 'You win!' + " " + playerScore++ + " point to the player"
+    if (computerChoice === 'scissors' && userChoice === 'rock')
+    results = 'You win!' + " " + playerScore++ + " point to the player"
+    
+    if (computerChoice === 'rock' && userChoice === 'scissors')
+    results = 'You loose!' + " " + computerScore++ + " point to the computer"
+    if (computerChoice === 'scissors' && userChoice === 'paper')
+    results = 'You loose!' + " " + computerScore++ + " point to the computer"
+    if (computerChoice === 'paper' && userChoice === 'rock')
+    results = 'You loose!' + " " + computerScore++ + " point to the computer"
+    if (computerScore > 5)
+    results = ' Game Over Computer wins!'
+    else if (playerScore > 5 )
+    results = ' Game Over You Win!'
+    result.innerHTML = results; 
+    
 }
